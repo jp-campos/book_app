21c5317b0a38a75dbeab73fed1158c4bc0a9f2ce
jpcampos <jpcampos@bancolombia.com.co>
2022-07-29 18:06:31 -0500

    added color fade animation, removed skeleton loading animation

--- lib/common/book_card/book_card.dart
+++ lib/common/book_card/book_card.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -21,15 +20,30 @@ class BookCard extends StatefulWidget {
   _BookCardState createState() => _BookCardState();
 }
 
-class _BookCardState extends State<BookCard> {
-  Color cardColor = Colors.blueGrey.shade200;
+const loadedAnimationDuration = 500;
+
+class _BookCardState extends State<BookCard>
+    with SingleTickerProviderStateMixin {
+  final defaultCardColor = Colors.blueGrey.shade200;
   ByteData? imageData;
   bool hasNoThumbnail = false;
 
+  late AnimationController controller;
+  late Color cardColor;
   @override
   void initState() {
     super.initState();
+    cardColor = defaultCardColor;
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: loadedAnimationDuration));
+
     fetchImage();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -50,7 +64,11 @@ class _BookCardState extends State<BookCard> {
       if (pic != null) {
         final avgColor = getAverageColor(pic);
         final tintAvgColor = lightenColor(avgColor);
-        setState(() => cardColor = tintAvgColor);
+        setState(() {
+          cardColor = tintAvgColor;
+          controller.forward();
+        });
+
         return;
       }
     }
@@ -61,72 +79,82 @@ class _BookCardState extends State<BookCard> {
 
   @override
   Widget build(BuildContext context) {
+    ColorTween? colorTween;
+    if (cardColor != defaultCardColor) {
+      colorTween = ColorTween(begin: defaultCardColor, end: cardColor);
+    }
     return GestureDetector(
       onTap: () => onBookTaped(widget.model),
-      child: Container(
-        decoration: ShapeDecoration(
-            shadows: [
-              BoxShadow(
-                  color: Colors.grey.shade300,
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 5))
-            ],
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  cardColor,
-                  cardColor,
-                ]),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15))),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTapDown: (_) => print('on tap down'),
-              onTap: () => onBookTaped(widget.model),
-              child: Transform.translate(
-                offset: Offset(hasNoThumbnail ? 0 : 10, -15),
-                child: Container(
-                  width: 60,
-                  height: 80,
-                  child: thumbnail(imageData),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        widget.model.title,
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                    ),
-                    Text(
-                      widget.model.author ?? '',
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      widget.model.firstPublishYear,
-                      overflow: TextOverflow.ellipsis,
-                    ),
+      child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, widget) {
+            return Container(
+              decoration: ShapeDecoration(
+                  shadows: [
+                    BoxShadow(
+                        color: Colors.grey.shade300,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 5))
                   ],
-                ),
-              ),
+                  color: colorTween?.evaluate(controller) ?? cardColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15))),
+              child: content(context),
+            );
+          }),
+    );
+  }
+
+  Row content(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTapDown: (_) => print('on tap down'),
+          onTap: () => onBookTaped(widget.model),
+          child: Transform.translate(
+            offset: Offset(hasNoThumbnail ? 0 : 10, -15),
+            child: Container(
+              width: 60,
+              height: 80,
+              child: thumbnail(imageData),
             ),
-          ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+            child: bookInfo(context),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column bookInfo(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Text(
+            widget.model.title,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        Text(
+          widget.model.author ?? '',
+          overflow: TextOverflow.ellipsis,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          widget.model.firstPublishYear,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
     );
   }
 
@@ -147,13 +175,14 @@ class _BookCardState extends State<BookCard> {
     }
 
     return AnimatedCrossFade(
-        firstChild: LoadingThumbnail(),
+        firstChild:
+            const Image(image: AssetImage('assets/cover_placeholder.png')),
         secondChild: hasImageLoaded
             ? Image.memory(byte.buffer.asUint8List())
             : SizedBox.shrink(),
         crossFadeState: hasImageLoaded
             ? CrossFadeState.showSecond
             : CrossFadeState.showFirst,
-        duration: Duration(milliseconds: 600));
+        duration: Duration(milliseconds: loadedAnimationDuration));
   }
 }
